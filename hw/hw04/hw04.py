6721130c2b4d623fe83492cec1f17d34cc084923
Eurekaimer <2507983039@qq.com>
2025-06-06 17:05:51 +0800

    hw04

--- hw/hw04/hw04.py
+++ hw/hw04/hw04.py
@@ -20,7 +20,11 @@ def deep_map(f, s):
     >>> s3 is s2[1]
     True
     """
-    "*** YOUR CODE HERE ***"
+    for i in range(len(s)):
+        if type(s[i]) == list:
+            deep_map(f, s[i])
+        else:
+            s[i] = f(s[i])
 
 
 HW_SOURCE_FILE=__file__
@@ -68,12 +72,12 @@ def end(s):
 def planet(mass):
     """Construct a planet of some mass."""
     assert mass > 0
-    "*** YOUR CODE HERE ***"
+    return ['planet', mass]
 
 def mass(p):
     """Select the mass of a planet."""
     assert is_planet(p), 'must call mass on a planet'
-    "*** YOUR CODE HERE ***"
+    return p[1]
 
 def is_planet(p):
     """Whether p is a planet."""
@@ -125,7 +129,14 @@ def balanced(m):
     >>> check(HW_SOURCE_FILE, 'balanced', ['Index'])
     True
     """
-    "*** YOUR CODE HERE ***"
+    if is_planet(m):
+        return True
+    else:
+        torque_left = length(left(m)) * total_mass(end(left(m)))
+        torque_right = length(right(m)) * total_mass(end(right(m)))
+        return torque_left == torque_right and balanced(end(left(m))) and balanced(end(right(m)))
+
+
 
 
 HW_SOURCE_FILE=__file__
@@ -140,7 +151,13 @@ def max_path_sum(t):
     >>> max_path_sum(t2) # 5, 2, 10
     17
     """
-    "*** YOUR CODE HERE ***"
+    if is_leaf(t):
+        return label(t)
+    max_branch_sum = 0
+    for branch in branches(t):
+        if max_path_sum(branch) > max_branch_sum:
+            max_branch_sum = max_path_sum(branch)
+    return label(t) + max_branch_sum
 
 
 
